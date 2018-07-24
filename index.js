571fbcc6c3590feecc880b347a28e5f6926bb569
Sara Vieira <hey@iamsaravieira.com>
2018-07-24 14:28:03 +0100

    add license

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const StylableWebpackPlugin = require("stylable-webpack-plugin")
+const StylableWebpackPlugin = require('stylable-webpack-plugin')
 
 const optimize = dev =>
   dev
     ? {}
     : {
-        removeUnusedComponents: true,
-        removeComments: true,
-        removeStylableDirectives: true,
-        classNameOptimizations: true,
-        shortNamespaces: true,
-        minify: true
-      }
+      removeUnusedComponents: true,
+      removeComments: true,
+      removeStylableDirectives: true,
+      classNameOptimizations: true,
+      shortNamespaces: true,
+      minify: true
+    }
 
 module.exports = (config, { _, dev }) => {
   config.plugins.push(
@@ -22,7 +22,7 @@ module.exports = (config, { _, dev }) => {
   )
 
   config.module.rules = config.module.rules.filter(
-    rule => !(rule.test || "").toString().includes("css")
+    rule => !(rule.test || '').toString().includes('css')
   )
 
   return config
