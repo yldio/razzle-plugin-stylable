ed72a96eed0eefe60283975e229a89a028d44632
Sara Vieira <hey@iamsaravieira.com>
2018-07-24 14:26:07 +0100

    fix default exportt

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
-const StylableWebpackPlugin = require('stylable-webpack-plugin')
+const StylableWebpackPlugin = require("stylable-webpack-plugin")
 
 const optimize = dev =>
   dev
     ? {}
     : {
-      removeUnusedComponents: true,
-      removeComments: true,
-      removeStylableDirectives: true,
-      classNameOptimizations: true,
-      shortNamespaces: true,
-      minify: true
-    }
+        removeUnusedComponents: true,
+        removeComments: true,
+        removeStylableDirectives: true,
+        classNameOptimizations: true,
+        shortNamespaces: true,
+        minify: true
+      }
 
-module.exports = {
-  modify: (config, { _, dev }) => {
-    config.plugins.push(
-      new StylableWebpackPlugin({
-        outputCSS: true,
-        includeCSSInJS: false,
-        optimize: optimize(dev)
-      })
-    )
+module.exports = (config, { _, dev }) => {
+  config.plugins.push(
+    new StylableWebpackPlugin({
+      outputCSS: true,
+      includeCSSInJS: false,
+      optimize: optimize(dev)
+    })
+  )
 
-    config.module.rules = config.module.rules.filter(
-      rule => !(rule.test || '').toString().includes('css')
-    )
+  config.module.rules = config.module.rules.filter(
+    rule => !(rule.test || "").toString().includes("css")
+  )
 
-    return config
-  }
+  return config
 }
